a940c281f1d398598f3fb7e088e171240d3e4852
PitToYondeKudasai <buttaronipeter@gmail.com>
2020-04-13 18:07:32 +0200

    Create Insertion Sort.py

---
+++ Insertion Sort.py
@@ -0,0 +1,22 @@
+####################
+## Insertion Sort ##
+####################
+# The following algorith implements the insertion sort
+# and prints at every iteration of the outer loop
+# the array so far
+
+def Insertion_sort(A):
+    for i in range(1,len(A)):
+        while A[i] < A[i-1]:
+            tmp = A[i]
+            A[i] = A[i-1]
+            A[i-1] = tmp
+            i -= 1
+        print(A)
+
+
+# Example
+A = [7,17,89,74,21,7,43,9,26,10]
+
+Insertion_sort(A)
+
